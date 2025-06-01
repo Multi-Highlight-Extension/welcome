81b45714e889d87174c3c42c3b6acf9c167f9198
keywordshightlighter <smith.helen@internet.ru>
2025-06-01 14:53:21 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import image1 from "./assets/image1.jpg";
+import image2 from "./assets/image2.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <h1>Multi Highlight Extension installed</h1>
+        <p className="description">
+          Click the puzzle piece (1) in the top right of your browser. Then,
+          click the little pin (2) next to the extension:
+        </p>
+        <img src={image1} height="auto" width="100%" alt="add extension" />
+        <p className="description">
+          Open the extension (3) on any page to start using Multi Highlight
+          Extension:
+        </p>
+        <img src={image2} height="auto" width="100%" alt="start using" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
